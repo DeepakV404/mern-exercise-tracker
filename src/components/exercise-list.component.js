c4706d67c0e1e489e3255c8a85afc182ae9d431d
Deepak <17deepakrv5@gmail.com>
2020-06-29 23:27:17 +0530

    Added React components to create and edit users and exercises

---
+++ src/components/exercise-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExerciseList extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on exercise list Component</p>
+            </div>
+        )
+    }
+}
